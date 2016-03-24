826806e60552f73e9d7e697cd4c1ab98ba7ac4a6
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-03-24 18:22:16 +0900

    テキストエディタを追加

---
+++ public/javascripts/ace-setting.js
@@ -0,0 +1,9 @@
+/**
+ * Created by s-sumi on 2016/03/24.
+ */
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.getSession().setMode("ace/mode/text");
+editor.setFontSize(16);
+editor.getSession().setUseWrapMode(true);
+editor.getSession().setTabSize(4);
