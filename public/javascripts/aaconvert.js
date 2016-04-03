cd95c7b196017ec782c665f395aa063c656dcde7
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-04-03 21:51:42 +0900

    リファクタリング

--- public/javascripts/aaconvert.js
+++ public/javascripts/aaconvert.js
@@ -4,34 +4,34 @@
 var endpoint='/';
 $(function() {
     $("#submit").on('click', function(evt) {
-        if(window.FormData){
-            if($("#input-20")[0].files[0]) {
-                var form = $("#settingForm").get()[0];
-                var formData = new FormData(form);
 
-                // Ajaxで送信
-                $.ajax({
-                    url: endpoint,
-                    method: 'post',
-                    dataType: 'json',
-                    // dataに FormDataを指定
-                    data: formData,
-                    // Ajaxがdataを整形しない指定
-                    processData: false,
-                    // contentTypeもfalseに指定
-                    contentType: false
-                }).done(function (data) {
-                    console.log('SUCCESS', data);
-                    editor.setValue(data.aa);
-                }).fail(function (jqXHR, textStatus, errorThrown) {
-                    console.log('ERROR', jqXHR, textStatus, errorThrown);
-                });
-            }else{
-                alert("ファイルを選択してください");
-            }
-        }else{
+        if(!window.FormData)
             alert("InternetExproler9以前には対応していません");
-        }
+
+        if(!$("#input-20")[0].files[0])
+            alert("ファイルを選択してください");
+
+        var form = $("#settingForm").get()[0];
+        var formData = new FormData(form);
+
+        // Ajaxで送信
+        $.ajax({
+            url: endpoint,
+            method: 'post',
+            dataType: 'json',
+            // dataに FormDataを指定
+            data: formData,
+            // Ajaxがdataを整形しない指定
+            processData: false,
+            // contentTypeもfalseに指定
+            contentType: false
+        }).done(function (data) {
+            console.log('SUCCESS', data);
+            editor.setValue(data.aa);
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            console.log('ERROR', jqXHR, textStatus, errorThrown);
+        });
+
 
         return false;
     });
