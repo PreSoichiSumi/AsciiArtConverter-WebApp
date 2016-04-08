8d73c3b3bbe701e7087ec11a0aa9f7c28ef555d7
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-04-09 04:35:21 +0900

    キャッシュ設定を変更

--- app/Filters.java
+++ app/Filters.java
@@ -41,7 +41,7 @@ public class Filters implements HttpFilters {
       // we're running in production or test mode then don't use any
       // filters at all.
       if (env.mode().equals(Mode.DEV)) {
-          return new EssentialFilter[] {customizedFilter, gzipFilter.asJava()};
+          return new EssentialFilter[] {gzipFilter.asJava()};
       } else {
          return new EssentialFilter[] {customizedFilter, gzipFilter.asJava()};
       }
