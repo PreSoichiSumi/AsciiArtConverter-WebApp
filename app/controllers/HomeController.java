878ae37175399f68e67830bd67ecc1ccae15c2f2
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-04-15 22:57:49 +0900

    全コードのreformat, import optimization

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -22,6 +22,7 @@ public class HomeController extends Controller {
     WebJarAssets webJarAssets;
     @Inject
     FormFactory formfactory;
+
     /**
      * An action that renders an HTML page with a welcome message.
      * The configuration in the <code>routes</code> file means that
@@ -29,34 +30,35 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(index.render("this is top page",webJarAssets));
+        return ok(index.render("this is top page", webJarAssets));
     }
 
     /**
      * ファイルはフォームデータの一部として送られてこない．
      * フォームのデータに添付されて送られてくる．
      * http://stackoverflow.com/questions/9452375/how-to-get-the-upload-file-with-other-inputs-in-play2#9587052
+     *
      * @return
      */
-    public Result aaConvert(){
-        MultipartFormData.FilePart picture=request().body().asMultipartFormData().getFile("picture");
+    public Result aaConvert() {
+        MultipartFormData.FilePart picture = request().body().asMultipartFormData().getFile("picture");
         //Map<String,String[]> form= request().body(). asMultipartFormData().asFormUrlEncoded();//checkboxは値がないときにはmapに要素すら無いので注意
-        DynamicForm form=formfactory.form().bindFromRequest();
-        if(picture!=null){
-            File file = (File)picture.getFile();
+        DynamicForm form = formfactory.form().bindFromRequest();
+        if (picture != null) {
+            File file = (File) picture.getFile();
 
-            String aa = ConvertionUtil.aaConvertion(file,form);
-            ObjectNode result= Json.newObject();
-            result.put("aa",aa);
+            String aa = ConvertionUtil.aaConvertion(file, form);
+            ObjectNode result = Json.newObject();
+            result.put("aa", aa);
             return ok(result);
         }
         return badRequest("picture is null");
     }
 
-    public boolean isPicture(File file){
-        String ext=file.getName().substring(file.getName().lastIndexOf(".")+1);
-        if(file.isFile() &&(
-                ext.equals("png") || ext.equals("jpeg") ||ext.equals("jpg")) )
+    public boolean isPicture(File file) {
+        String ext = file.getName().substring(file.getName().lastIndexOf(".") + 1);
+        if (file.isFile() && (
+                ext.equals("png") || ext.equals("jpeg") || ext.equals("jpg")))
             return true;
         return false;
     }
--- app/util/ConvertionUtil.java
+++ app/util/ConvertionUtil.java
@@ -25,94 +25,95 @@ import java.util.concurrent.Future;
  * Created by s-sumi on 2016/03/25.
  */
 public class ConvertionUtil {
-    public static String aaConvertion(File file,DynamicForm form){
+    public static String aaConvertion(File file, DynamicForm form) {
         BufferedImage bi;
         try {
             bi = ImageIO.read(file);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return "";
         }
         ConfigManager cm = generateConfigManager(form);
-        CharManager charm=new CharManager(cm);
-        PixelTable tmp=img2LineImg(bi,cm);
-        return lineImg2AA(tmp.data,tmp.width,tmp.height,charm,cm)[0];
+        CharManager charm = new CharManager(cm);
+        PixelTable tmp = img2LineImg(bi, cm);
+        return lineImg2AA(tmp.data, tmp.width, tmp.height, charm, cm)[0];
     }
-    public static ConfigManager generateConfigManager(DynamicForm form){
-        final int INDEX=6;
-        Map<String,String> tmp=new HashMap<>();
-        if(form.get("sel1").equals("NORESIZE")) {
+
+    public static ConfigManager generateConfigManager(DynamicForm form) {
+        final int INDEX = 6;
+        Map<String, String> tmp = new HashMap<>();
+        if (form.get("sel1").equals("NORESIZE")) {
             tmp.put("sizeType", "0");
             tmp.put("sizeImage_w", "0");
             tmp.put("sizeImage_h", "0");
-        }else{
-            String num=form.get("sel1").substring(INDEX);//substring->NOTNULL
+        } else {
+            String num = form.get("sel1").substring(INDEX);//substring->NOTNULL
 
-            if(num.equals(""))
+            if (num.equals(""))
                 throw new RuntimeException();
-            tmp.put("sizeType","1");
+            tmp.put("sizeType", "1");
 
-            String w,h;
-            switch (num){
+            String w, h;
+            switch (num) {
                 case "1":
-                    w="640";
-                    h="480";
+                    w = "640";
+                    h = "480";
                     break;
                 case "2":
-                    w="800";
-                    h="600";
+                    w = "800";
+                    h = "600";
                     break;
                 case "3":
-                    w="960";
-                    h="640";
+                    w = "960";
+                    h = "640";
                     break;
                 case "4":
-                    w="1024";
-                    h="768";
+                    w = "1024";
+                    h = "768";
                     break;
                 case "5":
-                    w="1280";
-                    h="960";
+                    w = "1280";
+                    h = "960";
                     break;
                 case "6":
-                    w="1920";
-                    h="1080";
+                    w = "1920";
+                    h = "1080";
                     break;
                 default:
                     throw new RuntimeException("switch-default");
             }
-            tmp.put("sizeImage_w",w);
-            tmp.put("sizeImage_h",h);
+            tmp.put("sizeImage_w", w);
+            tmp.put("sizeImage_h", h);
         }
-        tmp.put("accuracy",form.get("slider"));
-        tmp.put("lapRange",form.get("sel2"));
-        tmp.put("noiseLen","20");
-        tmp.put("connectRange","1");
-        tmp.put("fontName","MS Gothic");
-        tmp.put("fontSize","9");
-        tmp.put("pitch","0");
-        tmp.put("match","2");
-        tmp.put("score1","80");
-        tmp.put("score2","100");
-        tmp.put("multi","true");
-        tmp.put("matchCnt","1");
-        tmp.put("charSet","2");
-        if(form.get("tone")!=null){
-            tmp.put("tone","true");
-        }else {
+        tmp.put("accuracy", form.get("slider"));
+        tmp.put("lapRange", form.get("sel2"));
+        tmp.put("noiseLen", "20");
+        tmp.put("connectRange", "1");
+        tmp.put("fontName", "MS Gothic");
+        tmp.put("fontSize", "9");
+        tmp.put("pitch", "0");
+        tmp.put("match", "2");
+        tmp.put("score1", "80");
+        tmp.put("score2", "100");
+        tmp.put("multi", "true");
+        tmp.put("matchCnt", "1");
+        tmp.put("charSet", "2");
+        if (form.get("tone") != null) {
+            tmp.put("tone", "true");
+        } else {
             tmp.put("tone", "false");
         }
-        tmp.put("reversal","false");
-        tmp.put("toneValue","220");
-        tmp.put("toneTxt",":＠: ＠:  ＠. ");
-        tmp.put("textColor","0,0,0");
-        tmp.put("canvasColor","255,255,255");
-        tmp.put("angle","2");
-        tmp.put("useNotDir","true");
-        tmp.put("score3","80");
-        tmp.put("score4","100");
+        tmp.put("reversal", "false");
+        tmp.put("toneValue", "220");
+        tmp.put("toneTxt", ":＠: ＠:  ＠. ");
+        tmp.put("textColor", "0,0,0");
+        tmp.put("canvasColor", "255,255,255");
+        tmp.put("angle", "2");
+        tmp.put("useNotDir", "true");
+        tmp.put("score3", "80");
+        tmp.put("score4", "100");
 
-        ConfigManager cm=new ConfigManager();
+        ConfigManager cm = new ConfigManager();
         cm.setConfig(tmp);
 
 
@@ -120,43 +121,45 @@ public class ConvertionUtil {
         // -determine-operating-system-in-java
 
         //http://www.mltlab.com/wp/archives/67
-        String OS_NAME= System.getProperty("os.name").toLowerCase();
-        if(OS_NAME.startsWith("windows")){
-            if(form.get("font").equals("monospaced")){
-                cm.fontName="MS Gothic";
+        String OS_NAME = System.getProperty("os.name").toLowerCase();
+        if (OS_NAME.startsWith("windows")) {
+            if (form.get("font").equals("monospaced")) {
+                cm.fontName = "MS Gothic";
                 //cm.fontName="VL Gothic Regular";
-            }else{
-                cm.fontName="MS PGothic";
+            } else {
+                cm.fontName = "MS PGothic";
             }
-        }else if(OS_NAME.startsWith("linux")){
-            if(form.get("font").equals("monospaced")){
+        } else if (OS_NAME.startsWith("linux")) {
+            if (form.get("font").equals("monospaced")) {
                 //cm.fontName="Monospaced";
-                cm.fontName="VL Gothic Regular";
-            }else{
-                cm.fontName="SansSerif";
+                cm.fontName = "VL Gothic Regular";
+            } else {
+                cm.fontName = "SansSerif";
             }
-        }else{
-            cm.fontName="Arial";
+        } else {
+            cm.fontName = "Arial";
         }
 
 
-        cm.fontSize=9;
+        cm.fontSize = 9;
         return cm;
     }
-    public static PixelTable img2LineImg(BufferedImage img, ConfigManager cm){
-        int w=img.getWidth();
-        int h=img.getHeight();
+
+    public static PixelTable img2LineImg(BufferedImage img, ConfigManager cm) {
+        int w = img.getWidth();
+        int h = img.getHeight();
         if (cm.sizeType != ConfigManager.SizeType.NoResize) {
             w = cm.sizeImage.width;
             h = cm.sizeImage.height;
         }
-        PixelTable rawImg=PixelTable.createFromBufferedImage(img);
-        PixelTable zoomedImg= ImageUtil.zoomImage(rawImg,new Size(w,h),false);
-        PixelTable lineImg= ImageUtil.getLineImage(zoomedImg,
-                cm.accuracy,cm.lapRange,cm.noiseLen,cm.connectRange,cm);
-        return ImageUtil.zoomImage(lineImg,new Size(w,h),true);
+        PixelTable rawImg = PixelTable.createFromBufferedImage(img);
+        PixelTable zoomedImg = ImageUtil.zoomImage(rawImg, new Size(w, h), false);
+        PixelTable lineImg = ImageUtil.getLineImage(zoomedImg,
+                cm.accuracy, cm.lapRange, cm.noiseLen, cm.connectRange, cm);
+        return ImageUtil.zoomImage(lineImg, new Size(w, h), true);
     }
-    public static String[] lineImg2AA(int[] bmp, int width, int height, CharManager charm, ConfigManager cm){
+
+    public static String[] lineImg2AA(int[] bmp, int width, int height, CharManager charm, ConfigManager cm) {
         CharTable table = getTable(bmp, width, height);  //2値化画像取得？
         CharTable toneTable = getToneTable(bmp, width, height, cm);    //トーン用
 
@@ -170,12 +173,9 @@ public class ConvertionUtil {
         {
             lstSplitTable.add(trimTable(table, top, 0, h, w, 0));
 
-            if (cm.tone && cm.toneTxt.length > 0)
-            {
+            if (cm.tone && cm.toneTxt.length > 0) {
                 lstSplitToneTable.add(trimTable(toneTable, top, 0, h, w, 0));
-            }
-            else
-            {
+            } else {
                 lstSplitToneTable.add(new CharTable(0, 0));
             }
         }
@@ -219,7 +219,7 @@ public class ConvertionUtil {
             pool.shutdown();
         }
 
-        return new String[] { sbAA.toString(), sbAAType.toString() };
+        return new String[]{sbAA.toString(), sbAAType.toString()};
     }
 
     private static CharTable trimTable(CharTable table, int top, int left, int height, int width, int margin) {
@@ -240,7 +240,7 @@ public class ConvertionUtil {
 
     private static CharTable getTable(int[] bmp, int width, int height) {
         CharTable table = new CharTable(width, height);
-        for (int y = 0; y<table.height; y++) {
+        for (int y = 0; y < table.height; y++) {
             for (int x = 0; x < table.width; x++) {
                 if ((bmp[x + width * y] & 0xff) == 0) {
                     table.set(x, y, '■');
@@ -252,8 +252,7 @@ public class ConvertionUtil {
         return table;
     }
 
-    private static CharTable getToneTable(int[] bmp, int width, int height, ConfigManager cm)
-    {
+    private static CharTable getToneTable(int[] bmp, int width, int height, ConfigManager cm) {
         CharTable table = new CharTable(width, height);
         int val = 200 / cm.toneTxt.length;
         for (int y = 0; y < height; y++) {
@@ -269,40 +268,40 @@ public class ConvertionUtil {
         return table;
     }
 
-    public static ConfigManager generateConfigManager(){
-        final int INDEX=6;
-        Map<String,String> tmp=new HashMap<>();
+    public static ConfigManager generateConfigManager() {
+        final int INDEX = 6;
+        Map<String, String> tmp = new HashMap<>();
         tmp.put("sizeType", "0");
         tmp.put("sizeImage_w", "0");
         tmp.put("sizeImage_h", "0");
-        tmp.put("accuracy","50");
-        tmp.put("lapRange","9");
-        tmp.put("noiseLen","20");
-        tmp.put("connectRange","1");
-        tmp.put("fontName","MS Gothic");
-        tmp.put("fontSize","9");
-        tmp.put("pitch","0");
-        tmp.put("match","2");
-        tmp.put("score1","80");
-        tmp.put("score2","100");
-        tmp.put("multi","true");
-        tmp.put("matchCnt","1");
-        tmp.put("charSet","2");
+        tmp.put("accuracy", "50");
+        tmp.put("lapRange", "9");
+        tmp.put("noiseLen", "20");
+        tmp.put("connectRange", "1");
+        tmp.put("fontName", "MS Gothic");
+        tmp.put("fontSize", "9");
+        tmp.put("pitch", "0");
+        tmp.put("match", "2");
+        tmp.put("score1", "80");
+        tmp.put("score2", "100");
+        tmp.put("multi", "true");
+        tmp.put("matchCnt", "1");
+        tmp.put("charSet", "2");
         tmp.put("tone", "false");
-        tmp.put("reversal","false");
-        tmp.put("toneValue","220");
-        tmp.put("toneTxt",":＠: ＠:  ＠. ");
-        tmp.put("textColor","0,0,0");
-        tmp.put("canvasColor","255,255,255");
-        tmp.put("angle","2");
-        tmp.put("useNotDir","true");
-        tmp.put("score3","80");
-        tmp.put("score4","100");
+        tmp.put("reversal", "false");
+        tmp.put("toneValue", "220");
+        tmp.put("toneTxt", ":＠: ＠:  ＠. ");
+        tmp.put("textColor", "0,0,0");
+        tmp.put("canvasColor", "255,255,255");
+        tmp.put("angle", "2");
+        tmp.put("useNotDir", "true");
+        tmp.put("score3", "80");
+        tmp.put("score4", "100");
 
-        ConfigManager cm=new ConfigManager();
+        ConfigManager cm = new ConfigManager();
         cm.setConfig(tmp);
-        cm.fontName="Monospaced";
-        cm.fontSize=9;
+        cm.fontName = "Monospaced";
+        cm.fontSize = 9;
         return cm;
     }
 
